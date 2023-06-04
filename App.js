04bd98b972f52cc8d3a2e44bc540f7690821fa46
Shadia Kahin <kahinshadia@gmail.com>
2023-06-04 11:31:54 +0300

    second commit

--- App.js
+++ App.js
@@ -1,47 +1,19 @@
-import react from "react"
-import { View, Text, SafeAreaView, StyleSheet } from "react-native"
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import CurrentWeather from "./src/components/CurrentWeather";
+
 
 const App = () => {
-
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 8 </Text>
-          <Text style={styles.highLow}>Low: 6</Text>
-        </View>
-      </View>
-    </SafeAreaView>
-
-  )
+    <View style={styles.container}>
+      <CurrentWeather />
+    </View>
+)
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'pink',
-    flex: 1,
-    alignItems: 'center'
-  },
-  wrapper: {
-    flex: 1,
-  },
-  temp: {
-    color: 'black',
-    fontSize: 48
-  },
-  feels: {
-    fontSize: 30,
-    color: 'black'
-  },
-  highLow: {
-    color: 'black',
-    fontSize: 20
-  },
-  highLowWrapper: {
-    flexDirection: 'row'
+    flex: 1
   }
 })
-
 export default App;
